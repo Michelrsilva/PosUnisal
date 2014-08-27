993b716e3ae74943e38256639f7e024177da130c
Michelrsilva <michelrsilva@gmail.com>
2014-08-27 02:03:35 -0300

    Inserido falha teste

--- Integracao/test/src/br/michelrsilva/CalculadoraTest.java
+++ Integracao/test/src/br/michelrsilva/CalculadoraTest.java
@@ -26,7 +26,7 @@ public class CalculadoraTest {
 	@Test
 	public void divisaoBasica() {
 		Calculadora calc = new Calculadora(8,2);
-		assertEquals(4,calc.divisao()); 
+		assertEquals(3,calc.divisao()); 
 	}
 	
 	@Test(expected=Calculadora.NaoPodeDividirPorZero.class)
