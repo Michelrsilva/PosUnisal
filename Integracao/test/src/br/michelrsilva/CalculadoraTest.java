0f93b30553c2ac46df5de856ff31fe968b0257af
Michelrsilva <michelrsilva@gmail.com>
2014-08-27 02:01:08 -0300

    Alterado teste divisão basica

--- Integracao/test/src/br/michelrsilva/CalculadoraTest.java
+++ Integracao/test/src/br/michelrsilva/CalculadoraTest.java
@@ -25,8 +25,8 @@ public class CalculadoraTest {
 
 	@Test
 	public void divisaoBasica() {
-		Calculadora calc = new Calculadora(6,2);
-		assertEquals(3,calc.divisao()); 
+		Calculadora calc = new Calculadora(8,2);
+		assertEquals(4,calc.divisao()); 
 	}
 	
 	@Test(expected=Calculadora.NaoPodeDividirPorZero.class)
